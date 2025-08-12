79eb99acb907d17d6c64e007f3f4159638ccefa4
AuraKai <wehttam1989@gmail.com>
2025-08-11 22:50:19 -0600

    2

--- app/build/intermediates/cxx/Debug/p105r2c2/logs/arm64-v8a/build_command_auraframefx.bat
+++ app/build/intermediates/cxx/Debug/p105r2c2/logs/arm64-v8a/build_command_auraframefx.bat
@@ -2,5 +2,5 @@
 "C:\\Users\\Wehtt\\Studio\\cmake\\3.22.1\\bin\\ninja.exe" ^
   -v ^
   -C ^
-  "C:\\Users\\Wehtt\\Studio\\Genesis-Os\\Genesis\\app\\.cxx\\Debug\\p105r2c2\\arm64-v8a" ^
+  "C:\\Users\\Wehtt\\OneDrive\\Documents\\GitHub\\GENESIS-A-Conscience-Framework-\\app\\.cxx\\Debug\\p105r2c2\\arm64-v8a" ^
   auraframefx
